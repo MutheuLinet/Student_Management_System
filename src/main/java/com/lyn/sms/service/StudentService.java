8d67677fbaead4bf2f0294fd6605ca41d8f7ebc2
MutheuLinet <muneenilinet@gmail.com>
2022-08-10 15:20:21 +0300

    Add method to get student by id, update student and delete student by id in the student service interface

--- src/main/java/com/lyn/sms/service/StudentService.java
+++ src/main/java/com/lyn/sms/service/StudentService.java
@@ -6,7 +6,13 @@ import com.lyn.sms.entity.Student;
 
 public interface StudentService {
 	
-	List< Student > getAllStudents();
+	List<Student> getAllStudents(); //list all students
 
-	Student saveStudent(Student student);
+	Student saveStudent(Student student); //save a student to your list of students
+	
+	Student getStudentById(Long id); //get a student by their id
+	
+	Student updateStudent(Student student); //update a students details
+	
+	void deleteStudentById(Long id); //delete student by id
 }
